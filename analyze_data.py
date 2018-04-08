d909b0f8cdafedf771b6da86a103cfbc2df5ef67
Taylor Sweet <tsweet730@gmail.com>
2018-04-07 21:30:51 -0700

    Added functionality to analyze_data.py to convert strings of object/image tags to numerical feature vectors using a "Bag of Words" approach and with the support of the sci-kit learn toolbox. Also used the K-Nearest Neighbors algorithm with sci-kit learn to find the most closely related profiles given a target profile. Added scipy and sci-kit learn to requirements.txt.

--- analyze_data.py
+++ analyze_data.py
@@ -1,22 +1,45 @@
 import json
-import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.neighbors import NearestNeighbors
 
 # Directory that metadata is saved in
-metadata_directory = '/Users/Taylor/PycharmProjects/InfluencerConnect/users/'
+metadata_directory = './users/'
 
 # Load Instagram user list
-instagram_users = open('instagram_users2.txt').read().split('\n')
+instagram_users = open('instagram_users.txt').read().split('\n')
 
-features = []
+user_str = ''
+global_str = []
 
 # Loop over users
-for user, username in enumerate(instagram_users):
+for username in instagram_users:
 
     metadata = json.load(open(metadata_directory + username + '.json'))
 
-    for i in range(0, len(metadata)):
-        features.append(metadata[i]["image_contents"][0])
+    for i in range(1, len(metadata)):
+        if not metadata[i]['is_video']:
+            user_str += ' ' + metadata[i]['image_contents'][0]
 
-print(features)
+    global_str.append(user_str)
+    user_str = ''
 
-print('test')
+# Vectorize image content strings
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(global_str)
+
+# Fit K-Nearest Neighbors model
+neigh = NearestNeighbors(n_neighbors=5)
+neigh.fit(X)
+
+
+# Specify target brand profile position in 'instagram_user' list
+target = 10
+X_tilde = X[target]
+print('Target profile is:')
+print(instagram_users[target])
+
+# Find nearest neighbors
+neighbors = neigh.kneighbors(X_tilde)[1][0]
+print('\n' + 'Most closely related profiles are:')
+for j in range(1, len(neighbors)):
+    print(instagram_users[neighbors[j]])
